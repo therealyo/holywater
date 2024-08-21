dc0ee2a0f1a8c75b0c59e09f2d89fbd2d57cbd60
therealyo <dpshenik2002@gmail.com>
2024-08-21 19:33:00 +0300

    add config validation

--- src/comments/dto/get-comments.args.ts
+++ src/comments/dto/get-comments.args.ts
@@ -1,4 +1,3 @@
-// src/comments/dto/get-comments.args.ts
 import { ArgsType, Field } from '@nestjs/graphql';
 import { PaginationArgs } from 'src/common/pagination/cursor-based.input';
 import { IsUUID } from 'class-validator';
--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,17 +1,26 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule as NestConfigModule } from '@nestjs/config';
-// import { Environment } from './environment';
-// import  from './aws.provider';
+
+import * as Joi from '@hapi/joi';
+
+export const configValidationSchema = Joi.object({
+  NODE_ENV: Joi.string().valid('development', 'production', 'test').required(),
+  AWS_ACCESS_KEY_ID: Joi.string().required(),
+  AWS_SECRET_ACCESS_KEY: Joi.string().required(),
+  AWS_REGION: Joi.string().required(),
+  BUCKET_ENDPOINT: Joi.string().uri().required(),
+  CONTENT_BUCKET_NAME: Joi.string().required(),
+  POSTGRES_URL: Joi.string().uri().required(),
+  DYNAMODB_URL: Joi.string().uri().required(),
+  COMMENTS_TABLE_NAME: Joi.string().required(),
+});
 
 @Module({
   imports: [
     NestConfigModule.forRoot({
-      // validate: () => {
-      // throw new Error('Config not initalized');
-      // },
+      validationSchema: configValidationSchema,
       isGlobal: true,
       ignoreEnvFile: true,
-      // load: [awsConfig, postgresConfig],
     }),
   ],
 })
